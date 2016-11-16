639cde5bc7275b25fbcfd809682a6d243e0e1f6a
Pranali Rasal <pranalir212@gmail.com>
2016-11-16 10:06:37 +0530

    issue resolved

--- helloworld.c
+++ helloworld.c
@@ -2,6 +2,7 @@
 
 int main(){
 	printf ("Hello World!\n");
+	printf ("Error resolved\n");
 	printf ("This is a sample file created in PracticalEndSemesterDemo repository but in different branch\n");
 	return 0;
 }
